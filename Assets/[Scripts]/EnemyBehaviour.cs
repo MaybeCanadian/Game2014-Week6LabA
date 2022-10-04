cd756e93728ecb0b20584d33a8382194e31720e2
MaybeCanadian <1coffeye@gmail.com>
2022-10-04 00:40:58 -0400

    Commit 8 - added glow object to enemy

--- Assets/[Scripts]/EnemyBehaviour.cs
+++ Assets/[Scripts]/EnemyBehaviour.cs
@@ -5,26 +5,56 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     public Boundry bounds;
-    public Vector2 HorizontalSpeedBounds = new Vector2(2.0f, 6.0f);
+    public Boundry SpeedBounds;
+    public Boundry ScreenBounds;
 
     private float HorizontalSpeed;
+    private float VerticalSpeed;
+
+    public SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
-        float randomXPosition = Random.Range(bounds.XMin, bounds.XMax);
-        float randomYPosition = Random.Range(bounds.YMin, bounds.YMax);
-
-        HorizontalSpeed = Random.Range(HorizontalSpeedBounds.x, HorizontalSpeedBounds.y);
-
-        transform.position = new Vector3(randomXPosition, randomYPosition, 0.0f);
+        sr = GetComponent<SpriteRenderer>();
+        ResetEnemy();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+
         var HorizontalLength = bounds.XMax - bounds.XMin;
         transform.position = new Vector3(Mathf.PingPong(Time.time * HorizontalSpeed, HorizontalLength) - bounds.XMax,
-            transform.position.y, transform.position.z);
+            transform.position.y - VerticalSpeed * Time.deltaTime, transform.position.z);
+    }
+
+    private void CheckBounds()
+    {
+        if (transform.position.y < ScreenBounds.YMin)
+        {
+            ResetEnemy();
+        }
+    }
+
+    private void ResetEnemy()
+    {
+        float randomXPosition = Random.Range(bounds.XMin, bounds.XMax);
+        float randomYPosition = Random.Range(bounds.YMin, bounds.YMax);
+
+        transform.position = new Vector3(randomXPosition, randomYPosition, 0.0f);
+
+        HorizontalSpeed = Random.Range(SpeedBounds.XMax, SpeedBounds.XMin);
+        VerticalSpeed = Random.Range(SpeedBounds.YMax, SpeedBounds.YMin);
+
+        List<Color> ColorList = new List<Color>() { Color.red, Color.yellow, Color.magenta, Color.cyan, Color.white, Color.white };
+
+        sr.material.SetColor("_Color", ColorList[Random.Range(0, ColorList.Count)]);
     }
 }
